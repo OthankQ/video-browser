8bea92834d5a7127ff0b334e5b1dd55d947f5341
Ryan <kim1885@purdue.edu>
2019-04-12 23:57:50 -0400

    add videoList styling

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,17 +1,19 @@
+import "./VideoItem.css";
 import React from "react";
 
 const VideoItem = ({ video }) => {
   return (
-    <div
-      className="ui segment"
-      style={{ display: "flex", alignContent: "center" }}>
-      <a href="#">
-        <img
-          src={`${video.snippet.thumbnails.medium.url}`}
-          alt={`${video.snippet.description}`}
-        />
-      </a>
-      <a href={`${video.etag}`}>{video.snippet.title}</a>
+    <div className="video-item item">
+      <img
+        className="ui image"
+        src={`${video.snippet.thumbnails.medium.url}`}
+        alt={`${video.snippet.description}`}
+      />
+      <div className="content">
+        <a className="header" href={`${video.etag}`}>
+          {video.snippet.title}
+        </a>
+      </div>
     </div>
   );
 };
